5c53911d8fb9054c1256e1b175046500e88a76ef
Wagner Carestini <wcarestini@gmail.com>
2019-05-27 18:28:56 -0300

    Feita a refatoração do código, sem adicionar nenhuma funcionalidade, para assim manter o código mais legivel e facil de fazer manutenção, caso necessário

---
+++ js/calcula-imc.js
@@ -0,0 +1,39 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i =0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
+    var peso = paciente.querySelector(".info-peso").textContent;
+    
+    var altura = paciente.querySelector(".info-altura").textContent;
+    
+    var pesoValido = true;
+    var alturaValida = true;
+    var setaImc = paciente.querySelector(".info-imc");
+    
+    if(peso <= 0 || peso >= 1000){
+        pesoValido = false;
+        setaImc.textContent = "Peso inválido!";
+    }
+    if(altura <= 0 || altura >= 3.00){
+        alturaValida = false;
+        setaImc.textContent = "Altura inválida!";
+    }
+    if(pesoValido && alturaValida){
+        var imc = calculaImc(peso,altura);
+        setaImc.textContent = imc;
+        paciente.classList.add("paciente-valido");
+    } else if(pesoValido == false || alturaValida == false){
+        paciente.classList.add("paciente-invalido");
+    }
+}
+
+function calculaImc(peso,altura){
+    var imc = 0;
+
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
