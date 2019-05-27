b21bb670d3afa6ea8e0c872243d919a607bac051
Wagner Carestini <wcarestini@gmail.com>
2019-05-27 16:58:13 -0300

    Adicionado um pequeno formulario para facilitar a inclusão de novos pacientes para o usuario

--- js/principal.js
+++ js/principal.js
@@ -30,3 +30,37 @@ for(var i =0; i < pacientes.length; i++){
     }
 }
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var tdNome = document.createElement("td");
+    var tdPeso = document.createElement("td");
+    var tdAltura = document.createElement("td");
+    var tdGordura = document.createElement("td");
+
+    tdNome.textContent = nome;
+    tdPeso.textContent = peso;
+    tdAltura.textContent = altura;
+    tdGordura.textContent = gordura;
+
+    pacienteTr.appendChild(tdNome);
+    pacienteTr.appendChild(tdPeso);
+    pacienteTr.appendChild(tdAltura);
+    pacienteTr.appendChild(tdGordura);
+
+    var  tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+    console.log(pacienteTr);
+
+});
